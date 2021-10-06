9540e4d5c0d99026e3c147a0fa41aec65af3d04d
algorev <algorev@protonmail.com>
2021-10-06 16:04:57 +0200

    Remove tab system.

--- src/game/Game.hx
+++ src/game/Game.hx
@@ -1,10 +1,8 @@
 import js.html.TableSectionElement;
 import js.Browser;
-import Tabs;
 
 class Game {
 	static function main(){
 		trace(haxe.Resource.getString("storyText"));
-		var tabHandler = new Tabs();
 	}
 }
